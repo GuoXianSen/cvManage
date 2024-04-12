8df672c8ddb0f869154ac5323d6839e7771bef1a
GuoXianSen <775741842@qq.com>
2024-04-12 10:57:03 +0800

    Project Initializtion

---
+++ src/main/java/com/gyz/cvmanage/CvManageApplication.java
@@ -0,0 +1,13 @@
+package com.gyz.cvmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gyz/cvmanage/CvManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyz.cvmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
